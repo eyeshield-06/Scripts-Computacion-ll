91cd675ff84a03e53199a8732363043be0fc3c60
eyeshield-06 <rudymirandabas@gmail.com>
2022-11-11 16:51:31 -0300

    Nov11

---
+++ Ayudantia3part2.R
@@ -0,0 +1,24 @@
+## 2)
+Det <- function(A){
+    if(length(A) == 1){
+        return(A)
+    }
+    else{
+        temp <- 0
+        for(j in 1:sqrt(length(A))){ # dimension de una matriz cuadrada
+            temp <- temp + (-1)^(1+j)*A[1,j]*det(A[-1, -j])
+        }
+        return(temp)
+    }
+}
+# Falta mejorar para que reconozca que se le
+# estan ingresando otras estructuras de datos.
+# ademas de verificar que la matriz no es cuadrada.
+testeo <- matrix(data = c(-1, 1, 1, 1,
+                         1, -1, 1, 1,
+                         1, 1, -1, 1,
+                         1, 1, 1, -1), nrow = 4, ncol = 4)
+det(testeo) # funcion propia de R
+Det(testeo) # nuestra funcion
+
+
